cab8e668507f476b7ee524d5011109616d7a4c30
yutongwang1030-ctrl <yutongwang1030@gmail.com>
2026-08-06 12:18:45 +0800

    Upgrade site to cloud MVP

---
+++ assets/js/config.js
@@ -0,0 +1,6 @@
+window.APP_CONFIG = window.APP_CONFIG || {
+  SUPABASE_URL: "https://gfjehljokdxbucgltykc.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_n-ZHGrqY77-PMyOn24ivcg_-aRR8xwA",
+  SCORE_ENDPOINT: "/api/score",
+  ENABLE_MOCK_SCORING: true,
+};
